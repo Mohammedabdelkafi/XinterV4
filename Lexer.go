b5bd4244da7f0734399b73f4e9f4e0ae225cad86
Mohammed abdelkafi <kawruusir@gmail.com>
2025-02-06 13:21:39 +0100

    Working on the lexer part 2

--- Lexer.go
+++ Lexer.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	tokenize()
+	x := "let a  = 7"
+	slice(x)
 }
-
-func tokenize() {
-	str := "Hello World"
-	res := strings.Split(str, " ")
-	fmt.Printf("%v\n", res)
+func advance(text string) {
+	print("i'm bored")
 }
+func slice(text string) {
+	str := text
+	var res []string
+	for _, ch := range str {
+		res = append(res, fmt.Sprintf("%q", ch))
+	}
+	fmt.Println()
+}
+func Tokenize() {}
